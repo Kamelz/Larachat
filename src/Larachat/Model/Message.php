1caf6228bac7c02c46ee9dcfd5335e595e0e07fc
Kamelz <kamel1145@gmail.com>
2018-09-06 15:13:40 +0300

    Messages services

--- src/Larachat/Model/Message.php
+++ src/Larachat/Model/Message.php
@@ -6,11 +6,122 @@ use Illuminate\Database\Eloquent\Model;
 use Vendor\Larachat\Traits\Affiliateable;
 
 class Message extends Model{
+
+	/** @var instance  */
+    protected static $instance;
     
+    /** @var fillable */
     protected $fillable = [
         'to',
         'from',
         'message',
         'is_read',
     ];
+
+    /**
+     * Store sent message
+     * @param  User $from    User Id
+     * @param  User $to      User Id
+     * @param  String $message 
+     * @return Message          
+     */
+    public static function send($from,$to,$message){
+    	
+    	$from = gettype($from) === "object" ? $from->id : $from;
+
+    	$to = gettype($to) === "object" ? $to->id : $to;
+    	
+    	Message::create([
+    		'from' => $from,
+    		'to' => $to,
+    		'message' => $message,
+    	]);
+
+    	return static::getself();
+    }
+
+    /**
+     * @param  User $user
+     * @return Message      
+     */
+    public static function getUserMessages($user){
+
+    	$user = gettype($user) === "object" ? $user->id : $user;
+
+    	return Message::where('to',$user)->get();
+    }
+  
+    /**
+     * @param  Message $message
+     * @return Message      
+     */
+    public static function markAsRead(Message $message){
+
+    	$message->is_read = 1;
+    	$message->save();
+    	
+    	return static::getself();
+    }
+
+    /**
+     * @param  Message $message
+     * @return Message      
+     */
+    public static function markAsUnread(Message $message){
+
+    	$message->is_read = 0;
+    	$message->save();
+
+    	return static::getself();
+    }  
+
+    /**
+     * @param  Message $message
+     * @return Message      
+     */
+    public static function getReadMessages($user){
+
+        $user = gettype($user) === "object" ? $user->id : $user;
+
+        return Message::where('to' , $user)
+            ->where('is_read',1)
+            ->get();
+    }
+
+    /**
+     * @param  Message $message
+     * @return Message      
+     */
+    public static function getUnreadMessages($user){
+
+    	$user = gettype($user) === "object" ? $user->id : $user;
+
+    	return Message::where('to' , $user)
+    		->where('is_read',0)
+    		->get();
+    }
+
+    /**
+     * @param  User $user
+     * @return Message      
+     */
+    public static function getSentMessages($user){
+
+    	$user = gettype($user) === "object" ? $user->id : $user;
+
+    	return Message::where('from',$user)->get();
+    }
+
+    /**
+     * @return Message
+     */
+    protected static function getself()
+    {
+        if (static::$instance === null) 
+        {
+            static::$instance = new Message;
+        }
+
+        return static::$instance;
+    }
 }
--- tests/Unit/ChatTest.php
+++ tests/Unit/ChatTest.php
@@ -1,17 +1,157 @@
 <?php
 
 namespace Vendor\Larachat\Tests\Unit;
-
+use Vendor\Larachat\Model\Message;
+use Vendor\Larachat\Tests\TestModels\User;
 use Vendor\Larachat\Tests\LarachatTestCase;
 
 class ChatTest extends LarachatTestCase
 {
+	/**
+	* Setup the test environment.
+	*
+	* @return void
+	*/
+    protected function setUp(){
+
+		parent::setUp();
+    }
+
     /**
      * @test
      * @return void
      */
     public function it_can_send_message_to_user()
     {
+        $message = $this->makeMessage();
+        $messages = Message::first();
+
+        $this->assertSame($message['from']->id,$messages->from);
+        $this->assertSame($message['to']->id,$messages->to);
+        $this->assertSame($message['content'],$messages->message);
+    }
+
+    /**
+     *
+     * @test
+     * @return void
+     */
+    public function it_can_receive_all_messages()
+    {
+        $message = $this->makeMessage();   
+        $messages = Message::getUserMessages($message['to']);
+        dd($messages);
+        $this->assertSame($message['from']->id,$messages[0]['from']);
+        $this->assertSame($message['to']->id,$messages[0]['to']);
+        $this->assertSame($message['content'],$messages[0]['message']);
+    }
+
+    /**
+     *
+     * @test
+     * @return void
+     */
+    public function it_can_receive_sent_messages()
+    {
+        $message = $this->makeMessage();
+      
+        $messages = Message::getSentMessages($message['from']);
+        $this->assertSame($message['from']->id,$messages[0]['from']);
+        $this->assertSame($message['to']->id,$messages[0]['to']);
+        $this->assertSame($message['content'],$messages[0]['message']);
+    }
+
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_mark_message_as_read()
+    {
+        $message = $this->makeMessage();
+        Message::markAsRead($message['model']);
+        $this->assertSame(1,$message['model']->fresh()->is_read);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_mark_message_as_unread()
+    {
+        $message = $this->makeMessage();
+        Message::markAsUnread($message['model']);
+        $this->assertSame(0,$message['model']->fresh()->is_read);
+    }
+
+
+    /**
+     * @todo
+     * @test
+     * @return void
+     */
+    public function it_can_receive_read_messages()
+    {
+
+        $message = $this->makeMessage();
+
+        Message::markAsRead($message['model']);
+
+        //unread message
+        Message::send(
+            $message['from'],
+            $message['to'],
+            'unread_message'
+        );
+
+        $messages = Message::getReadMessages($message['to']);
+
+        $this->assertSame(1,$messages->count());
+        $this->assertNotSame('unread_message',$messages->first()->message);
+    }
+
+    /**
+     * @todo
+     * @test
+     * @return void
+     */
+    public function it_can_receive_unread_messages()
+    {
+        $message = $this->makeMessage();
         
+        Message::markAsUnread($message['model']);
+
+           //unread message
+        Message::send(
+            $message['from'],
+            $message['to'],
+            'read_message'
+        );
+
+        $readMessage = Message::where('message','read_message')->first();
+        $readMessage->is_read = 1;  
+        $readMessage->save();  
+
+        $messages = Message::getUnreadMessages($messages['model']);
+
+        $this->assertSame(1,$messages->count());
+        $this->assertNotSame('read_message',$messages->first()->message);
+    }
+
+    public function makeMessage(){
+        $message = [
+            'from'    => $this->createUser(),
+            'to'      => $this->createUser(),
+            'content' => "test_message"
+        ];
+        Message::send(
+            $message['from'],
+            $message['to'],
+            $message['content']
+        );
+
+        $model = Message::where('to',$message['to']->id)->first();
+
+        return ['model' => $model] + $message;
     }
 }
