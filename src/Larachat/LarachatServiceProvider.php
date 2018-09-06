735564d104fb3b6018d7c68abc6d447b68ba4a52
Kamelz <kamel1145@gmail.com>
2018-09-06 15:12:25 +0300

    Refactor Service provider

--- src/Larachat/LarachatServiceProvider.php
+++ src/Larachat/LarachatServiceProvider.php
@@ -5,31 +5,17 @@ namespace Vendor\Larachat;
 use Illuminate\Support\ServiceProvider;
 use Vendor\Larachat\Commands\FooCommand;
 
-/**
- * A Laravel 5.5 Larachat boilerplate
- *
- * @author: Rémi Collin (remi@code16.fr)
- */
+
 class LarachatServiceProvider extends ServiceProvider {
 
     /**
      * This will be used to register config & view in 
      * your package namespace.
      *
-     * --> Replace with your package name <--
      * 
      * @var  string
      */
     protected $packageName = 'Larachat';
-
-    /**
-     * A list of artisan commands for your package
-     * 
-     * @var array
-     */
-    protected $commands = [
-        FooCommand::class,
-    ];
 
     /**
      * Bootstrap the application services.
@@ -38,38 +24,14 @@ class LarachatServiceProvider extends ServiceProvider {
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/routes.php');
-
-        // Register Views from your package
-        $this->loadViewsFrom(__DIR__.'/../views', $this->packageName);
-
         // Regiter migrations
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
-        // Register translations
-        $this->loadTranslationsFrom(__DIR__.'/../lang', $this->packageName);
-        $this->publishes([
-            __DIR__.'/../lang' => resource_path('lang/vendor/'. $this->packageName),
-        ]);
+            $this->publishes([
+                __DIR__.'/../database/migrations' => public_path('vendor/Larachat'),
+            ], 'public');
 
-        // Register your asset's publisher
-        $this->publishes([
-            __DIR__.'/../assets' => public_path('vendor/'.$this->packageName),
-        ], 'public');
 
-        // Publish your seed's publisher
-        $this->publishes([
-            __DIR__.'/../database/seeds/' => base_path('/database/seeds')
-        ], 'seeds');
-
-        // Publish your config
-        $this->publishes([
-            __DIR__.'/../config/config.php' => config_path($this->packageName.'.php'),
-        ], 'config');
-
-        if ($this->app->runningInConsole()) {
-            $this->commands($this->commands);
-        }
     }
 
     /**
@@ -79,9 +41,6 @@ class LarachatServiceProvider extends ServiceProvider {
      */
     public function register()
     {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/config.php', $this->packageName
-        );
 
     }
 
